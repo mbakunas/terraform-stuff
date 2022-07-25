263116e3f63f636b0081b42d6010cdcf3e674489
Mark Bakunas <mbakunas@users.noreply.github.com>
2022-07-25 17:01:40 -0400

    Run init

---
+++ labs_dev_2_1/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.15.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "labs_dev_2_rg" {
+  name     = "labs_dev_2_rg"
+  location = "eastus2"
+}
+
+resource "azurerm_storage_account" "labsdev2" {
+    name                     = "labsdev2"
+    resource_group_name      = azurerm_resource_group.labs_dev_2_rg.name
+    location                 = azurerm_resource_group.labs_dev_2_rg.location
+    access_tier              = "Standard"
+    account_replication_type = "GRS"
+}
