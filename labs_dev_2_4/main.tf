79e2f85ec279a68e01fb7e0d288762be527097f6
Mark Bakunas <mbakunas@users.noreply.github.com>
2022-07-28 17:26:34 -0400

    Day 4

--- labs_dev_2_4/main.tf
+++ labs_dev_2_4/main.tf
@@ -22,3 +22,30 @@ resource "azurerm_resource_group" "labs_dev_2_4_rg" {
   name     = "labs_dev_2_4_rg"
   location = "eastus2"
 }
+
+resource "azurerm_service_plan" "bakunastflab4_plan" {
+  name                = "bakunastflab4_plan"
+  resource_group_name = azurerm_resource_group.labs_dev_2_4_rg.name
+  location            = azurerm_resource_group.labs_dev_2_4_rg.location
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+resource "azurerm_linux_web_app" "bakunastflab4" {
+  name                = "bakunastflab4"
+  resource_group_name = azurerm_resource_group.labs_dev_2_4_rg.name
+  location            = azurerm_resource_group.labs_dev_2_4_rg.location
+  service_plan_id     = azurerm_service_plan.bakunastflab4_plan.id
+/*
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io/v1"
+  }
+*/
+
+  site_config {
+    application_stack {
+        docker_image     = "nginx"
+        docker_image_tag = "latest"
+    }
+  }
+}
